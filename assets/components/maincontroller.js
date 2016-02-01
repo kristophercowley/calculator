5ffd0f867c6a95203381539d0bee1d4d91161d07
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-02-01 15:21:38 -0700

    Working Miles/km and F/C conversion

--- assets/components/maincontroller.js
+++ assets/components/maincontroller.js
@@ -1,10 +1,37 @@
 app.controller('MainController',function(){
-	this.test = "Testing, 1,2,3"
-	this.home = function(view){
-		if(view === ){
-			this. 	=
+	this.test = "Testing, 1,2,3";
+	this.m2k = function(miles){
+		
+	}
+	//Sets View Model on Button Click and clears old view
+	this.activePage = function(currentView){
+		if(currentView === this.view){
+			this.view = "";
 		}else{
-			this.	="";
+			this.view = currentView;
 		}
+	}
+	//Miles and Km conversion//Need to fix exact math, currentl roughed in
+	this.milesToKm = function(miles){
+		console.log("test")
+		this.mToKm = miles * 1.6;
+		console.log(this.mToKm);
+	}
+	this.kmToM = function(km){
+		console.log("test2")
+		this.kmToM = km / 1.6;
+		console.log(this.kmToM);
+	}
+	
+	//Celcius and F conversion
+	this.farToC = function(f){
+		console.log("testF")
+		this.fToC = (f - 32) / 1.8;
+		console.log(this.fToC);
+	}
+	this.celciusToF= function(c){
+		console.log("testc")
+		this.cToF = (c * 1.8) + 32 ;
+		console.log(this.cToF);
 	}
 })
