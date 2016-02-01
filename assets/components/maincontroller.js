ced5c7e7ab2e37ebf39e71f6dd041f88c50f9e9e
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-02-01 13:06:01 -0700

    Buttons

--- assets/components/maincontroller.js
+++ assets/components/maincontroller.js
@@ -1,3 +1,10 @@
 app.controller('MainController',function(){
-	this.test = "LKJHGFDSADFGHJK"
+	this.test = "Testing, 1,2,3"
+	this.home = function(view){
+		if(view === ){
+			this. 	=
+		}else{
+			this.	="";
+		}
+	}
 })
